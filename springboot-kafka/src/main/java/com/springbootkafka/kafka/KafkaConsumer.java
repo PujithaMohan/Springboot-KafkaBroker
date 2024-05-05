6334d5bc1dbffffd7db65187f9ed13600207ab87
Pujitha <Pujitha.polu@cirium.com>
2024-05-05 20:56:39 +0100

    Added Kafka Producer and Consumer Serializer and Deserializer and externalized configurations

--- springboot-kafka/src/main/java/com/springbootkafka/kafka/KafkaConsumer.java
+++ springboot-kafka/src/main/java/com/springbootkafka/kafka/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.springbootkafka.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
     public static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    @KafkaListener(topics = "topic1", groupId = "myGroup")
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message){
         LOGGER.info(String.format("Message received %s",message));
     }
--- springboot-kafka/src/main/java/com/springbootkafka/kafka/KafkaProducer.java
+++ springboot-kafka/src/main/java/com/springbootkafka/kafka/KafkaProducer.java
@@ -1,11 +1,14 @@
 package com.springbootkafka.kafka;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 @Service
 public class KafkaProducer {
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
     private static final Logger LOGGER= LoggerFactory.getLogger(KafkaProducer.class);
 
     public KafkaProducer(final KafkaTemplate<String, String> kafkaTemplate) {
@@ -16,6 +19,6 @@ public class KafkaProducer {
 
     public void sendMessage(String message){
         LOGGER.info(String.format("#### -> Producing message -> %s",message));
-        kafkaTemplate.send("topic1",message);
+        kafkaTemplate.send(topicName,message);
     }
 }
